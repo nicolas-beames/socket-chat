563d5c45a4f73940546b00167c29d0516326435f
Filipe <filipemzanin@gmail.com>
2025-05-21 20:02:04 -0300

    feat: updates encodes

--- client.py
+++ client.py
@@ -9,9 +9,9 @@ client.connect(('127.0.0.1', 55555))
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('utf-8')
             if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+                client.send(nickname.encode('utf-8'))
             else:
                 print(message)
         except:
@@ -22,7 +22,7 @@ def receive():
 def write():
     while True:
         message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
+        client.send(message.encode('utf-8'))
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def handle(client):
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            broadcast(f'{nickname} left the chat!'.encode('utf-8'))
             nicknames.remove(nickname)
             break
 
@@ -34,14 +34,14 @@ def receive():
         client, address = server.accept()
         print(f"Connected with {str(address)}")
 
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
         nicknames.append(nickname)
         clients.append(client)
 
         print(f"Nickname of the client is {nickname}!")
-        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
-        client.send('Connected to the server!'.encode('ascii'))
+        broadcast(f'{nickname} joined the chat!'.encode('utf-8'))
+        client.send('Connected to the server!'.encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
